d95d5fc58a2009fcc0a4f04b56eb796656cba9b9
Rick South <rick@natamus.com>
2023-12-12 00:25:41 +0100

    Rain Be Gone Ritual source code for 1.20.1.

---
+++ Forge/src/main/java/com/natamus/rainbegoneritual/ModForge.java
@@ -0,0 +1,32 @@
+package com.natamus.rainbegoneritual;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.rainbegoneritual.forge.events.ForgeRitualEvent;
+import com.natamus.rainbegoneritual.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeRitualEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}

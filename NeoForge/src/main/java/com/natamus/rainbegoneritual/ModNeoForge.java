25466bf1d42ecd755c4dd9f942a5eba96b704b9e
Rick South <rick@natamus.com>
2023-12-12 00:25:54 +0100

    Rain Be Gone Ritual source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/rainbegoneritual/ModNeoForge.java
@@ -0,0 +1,32 @@
+package com.natamus.rainbegoneritual;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.rainbegoneritual.neoforge.events.NeoForgeRitualEvent;
+import com.natamus.rainbegoneritual.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeRitualEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}

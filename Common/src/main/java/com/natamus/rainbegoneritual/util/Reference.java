48511332219ddd67f4c821ef897d1a72ba5fc0bd
Rick South <rick@natamus.com>
2023-12-12 00:25:20 +0100

    Rain Be Gone Ritual source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/rainbegoneritual/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.rainbegoneritual.util;
+
+public class Reference {
+	public static final String MOD_ID = "rainbegoneritual";
+	public static final String NAME = "Rain Be Gone Ritual";
+	public static final String VERSION = "3.1";
+	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
+}

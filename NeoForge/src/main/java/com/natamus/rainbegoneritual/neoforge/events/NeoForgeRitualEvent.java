6d99d3049a94afe74c265b7e23c9b5ec361c6f39
Rick South <rick@natamus.com>
2024-06-26 00:01:33 +0200

    (v3.3) (1.21.0 Neo) Updated mod to work with NeoForge version 21.0.31+.

--- NeoForge/src/main/java/com/natamus/rainbegoneritual/neoforge/events/NeoForgeRitualEvent.java
+++ NeoForge/src/main/java/com/natamus/rainbegoneritual/neoforge/events/NeoForgeRitualEvent.java
@@ -2,10 +2,10 @@ package com.natamus.rainbegoneritual.neoforge.events;
 
 import com.natamus.rainbegoneritual.events.RitualEvent;
 import net.minecraft.world.entity.Entity;
-import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 @EventBusSubscriber
 public class NeoForgeRitualEvent {
@@ -15,7 +15,7 @@ public class NeoForgeRitualEvent {
 	}
 	
 	@SubscribeEvent
-	public static void onExplosionDamage(LivingHurtEvent e) {
+	public static void onExplosionDamage(LivingIncomingDamageEvent e) {
 		Entity entity = e.getEntity();
 		if (RitualEvent.onExplosionDamage(entity.level(), entity, e.getSource(), e.getAmount()) == 0.0F) {
 			e.setCanceled(true);

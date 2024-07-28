80d28b0cb2a0c6b7fc6685a48021f0526bea2943
Rick South <rick@natamus.com>
2024-07-28 07:19:03 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/rainbegoneritual/ModForge.java
+++ Forge/src/main/java/com/natamus/rainbegoneritual/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.rainbegoneritual;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.rainbegoneritual.forge.events.ForgeRitualEvent;
 import com.natamus.rainbegoneritual.util.Reference;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,6 +14,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 

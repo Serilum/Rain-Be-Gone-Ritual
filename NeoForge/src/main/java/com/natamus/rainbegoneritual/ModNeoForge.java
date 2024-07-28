6a5b76707823d79ea22391a7320d266e71f81fcc
Rick South <rick@natamus.com>
2024-07-28 07:19:07 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/rainbegoneritual/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/rainbegoneritual/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.rainbegoneritual;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.rainbegoneritual.neoforge.events.NeoForgeRitualEvent;
 import com.natamus.rainbegoneritual.util.Reference;
 import net.neoforged.neoforge.common.NeoForge;
@@ -13,6 +14,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();

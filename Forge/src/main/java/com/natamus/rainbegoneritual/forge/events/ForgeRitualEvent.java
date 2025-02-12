7808efe2db5a154525efac03bdd920c8ab075205
Rick South <rick@natamus.com>
2025-02-12 23:18:43 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/rainbegoneritual/forge/events/ForgeRitualEvent.java
+++ Forge/src/main/java/com/natamus/rainbegoneritual/forge/events/ForgeRitualEvent.java
@@ -5,17 +5,15 @@ import net.minecraft.world.entity.Entity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeRitualEvent {
 	@SubscribeEvent
-	public void onClick(PlayerInteractEvent.RightClickBlock e) {
+	public static void onClick(PlayerInteractEvent.RightClickBlock e) {
 		RitualEvent.onClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
 	}
 	
 	@SubscribeEvent
-	public void onExplosionDamage(LivingHurtEvent e) {
+	public static void onExplosionDamage(LivingHurtEvent e) {
 		Entity entity = e.getEntity();
 		if (RitualEvent.onExplosionDamage(entity.level(), entity, e.getSource(), e.getAmount()) == 0.0F) {
 			e.setCanceled(true);

9cd4dc63b2bd3e36afc0f5283fe1bd435f813ae5
Rick South <rick@natamus.com>
2025-10-08 01:01:13 +0200

    Rain Be Gone Ritual source code for 1.21.10.

---
+++ NeoForge/src/main/java/com/natamus/rainbegoneritual/neoforge/events/NeoForgeRitualEvent.java
@@ -0,0 +1,22 @@
+package com.natamus.rainbegoneritual.neoforge.events;
+
+import com.natamus.rainbegoneritual.events.RitualEvent;
+import net.minecraft.world.entity.Entity;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+
+public class NeoForgeRitualEvent {
+	@SubscribeEvent
+	public static void onClick(PlayerInteractEvent.RightClickBlock e) {
+		RitualEvent.onClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+	
+	@SubscribeEvent
+	public static void onExplosionDamage(LivingIncomingDamageEvent e) {
+		Entity entity = e.getEntity();
+		if (RitualEvent.onExplosionDamage(entity.level(), entity, e.getSource(), e.getAmount()) == 0.0F) {
+			e.setCanceled(true);
+		}
+	}
+}
